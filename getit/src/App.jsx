c8539928d0a04eb7990ef6238b31d2b51d5df232
Naveen pandian <npandian515@gmail.com>
2025-01-10 19:30:40 +0530

    gsap homepage

--- getit/src/App.jsx
+++ getit/src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount } from 'wagmi';
 import PageNotFound from './Components/PageNotFound';
-
+import Menu from "../src/Components/Menu/Menu"
 function App() {
   const {address, isConnected} = useAccount();
 
   return (
     <>
-    <PageNotFound/>
-      <ConnectButton/>
+       <Menu />
+      
     </>
   )
 }
